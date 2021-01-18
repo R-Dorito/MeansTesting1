4ab49bcf729bad3d5f9f6665b83062a46089dea2
RITA UY <rita.uy56@gmail.com>
2021-01-18 17:09:24 +1100

    Changed the way the functions calculated. Also included a Define function that sets a global variable of common properties.

--- MeansTesting1/MeansTesting1.py
+++ MeansTesting1/MeansTesting1.py
@@ -1,20 +1,25 @@
 listNumbers = [0,1,1,1,2,2,4,4,7]
 
+# Use this to define the properties of the list
+def defineList(listNum):
+    listNum.sort()
+    
+    global listLength 
+    listLength = len(newList)
+
+
 def findMean(listNum):
     sum = 0
     for i in listNum:
-        sum += float(i)
+        sum += i
+    average = sum / listLength
 
-    count = float(len(listNum))
-    return sum/count
+    return average
 
 def truncatedMean(listNum):
     newList = list(dict.fromkeys(listNum))
-    newList.sort()
 
-    lastNumberOfList = len(newList) - 1
-
-    del newList[lastNumberOfList]
+    del newList[listLength - 1]
     del newList[0]
 
     return newList
@@ -23,6 +28,8 @@ def findMedian(listNum):
     lenList = len(listNum)
     # if odd do: x[(n+1)/2]
     # else do: ((x[n/s] + x[(n+1)/2])/2)
+
+
 
 
 
