9c396f04c0b00cb759e8159b2230778fd1aa981d
RITA UY <rita.uy56@gmail.com>
2021-01-18 17:32:21 +1100

    Median Included

--- MeansTesting1/MeansTesting1.py
+++ MeansTesting1/MeansTesting1.py
@@ -1,38 +1,56 @@
-listNumbers = [0,1,1,1,2,2,4,4,7]
-
 # Use this to define the properties of the list
 def defineList(listNum):
+    global LISTLENGTH 
     listNum.sort()
-    
-    global listLength 
-    listLength = len(newList)
-
+    LISTLENGTH = len(listNum)
 
 def findMean(listNum):
     sum = 0
     for i in listNum:
         sum += i
-    average = sum / listLength
-
+    average = sum / LISTLENGTH
     return average
 
 def truncatedMean(listNum):
     newList = list(dict.fromkeys(listNum))
-
-    del newList[listLength - 1]
+    del newList[len(newList) - 1]
     del newList[0]
-
     return newList
 
 def findMedian(listNum):
-    lenList = len(listNum)
     # if odd do: x[(n+1)/2]
-    # else do: ((x[n/s] + x[(n+1)/2])/2)
+    # where x is a set and 
+    # n is the number of elements
+    # else do: ((x[n/2] + x[(n+1)/2])/2)
 
+    if (LISTLENGTH % 2) != 0:
+        print("its odd")
 
+        # Although the formula states the n+1/2 should be done, 
+        # it does not work well in this case
+        location = (LISTLENGTH) / 2
+        median = listNum[int(location)]
+    else:
+        print("it's even")
+        
+        # Get the value of the first and second median number
+        location1 = listNum[int((LISTLENGTH - 1) / 2)] 
+        #print("L1:", location1, "at:",int((LISTLENGTH) / 2))
+        
+        location2 = listNum[int((LISTLENGTH) / 2)]
+        #print("L2:", location2, "at:", int((LISTLENGTH - 1) / 2))
 
+        median = (location1 + location2) / 2
+
+    print("the median is:", median)
+    return median
 
 
     
+listNumbers = [1, 1, 2, 6, 6, 9 ]
+defineList(listNumbers)
+
+
 #findMean(listNumbers)
 truncatedMean(listNumbers)
+findMedian(listNumbers)
