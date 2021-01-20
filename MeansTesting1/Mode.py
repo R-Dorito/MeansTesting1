72fef33ee67086a9ec5f18b0e554bd251a4dc8bd
RITA UY <rita.uy56@gmail.com>
2021-01-20 13:12:49 +1100

    Mode is now in a new class

--- MeansTesting1/Mode.py
+++ MeansTesting1/Mode.py
@@ -1,13 +1,18 @@
-class Mode(object):
+class Mode:
 #Add class name
 # break the bottom part up
 # Introduce matplot
 # Graphically draw this up
 
+    #This is a test Function
     def findMode(listNum):
-        # This here collects all the unique values of a given list
+        dictionaryMode = createModeDictionary(listNum)
+        frequency = returnMaxFrequency(dictionaryMode)
+        findAllModes(dictionaryMode, frequency)
+        
+    # This here collects all the unique values of a given list
+    def createModeDictionary(listNum):       
         #newList = list(dict.fromkeys(listNum))
-
         # List of numbers and their frequencies
         numberDictionary = []
 
@@ -15,24 +20,28 @@ class Mode(object):
         for createCountList in UNIQUELIST:
             modeOccurances = listNum.count(createCountList)        
             numberDictionary.append({'Value': createCountList, 'Count': modeOccurances})
+        return numberDictionary
 
-        # Used to return the largest frequency
+    # Used to return the largest frequency
+    def returnMaxFrequency(dictionaryMode):
         maxCount = 0
-        modeList = []
-        for itterable in numberDictionary:
+        for itterable in dictionaryMode:
             currentCount = itterable['Count']
             #print("Current Count is:", currentCount)
             if currentCount > maxCount:
                 maxCount = currentCount
         print("Max Value is:", maxCount)
+        return maxCount
 
-        # Used to find all values with "Largest Frequencies"
-        for theMode in numberDictionary:
-            currentComparison = theMode['Count']
-            if maxCount == currentComparison:
-                modeList.append(theMode)
-    
+    # Used to find all values with "Largest Frequencies"
+    def findAllModes(dictionaryMode, frequency):
+        modeList = []
+        for currentValuesInDict in dictionaryMode:
+            currentComparison = allValuesInDict['Count']
+            if frequency == currentComparison:
+                modeList.append(currentValuesInDict)
         print("There is", len(modeList), "Mode(s)")
+        return modeList
 
 
 
